be755328edaf42367453d945763110c4d3dd70e9
Michael Kubicki <Kubicki-Michael@live.de>
2019-06-23 13:51:18 +0200

    ColorSlime & BaseCritDamage

---
+++ Assets/Scripts/Enemy/ColorSlime.cs
@@ -0,0 +1,21 @@
+﻿namespace Enemy {
+	using Battle;
+
+	using UnityEngine;
+
+	public class ColorSlime : SingleEnemy
+	{
+		public Color[] RandomColors;
+		
+		public override void Spawn(Battlefield battlefield)
+		{
+			if (!battlefield.CanSpawnEnemy())
+				return;
+			
+			var pos = battlefield.GetNewEnemyLocation();
+
+			var me = Instantiate(this, pos, Quaternion.identity, battlefield.transform);
+			me.GetComponent<SpriteRenderer>().color = this.RandomColors[Random.Range(0, this.RandomColors.Length)];
+		}
+	}
+}
